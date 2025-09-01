f8f27a643a2822ddd4c5b9314b855897c58cc939
anamolhasan <mdanamolhasan1998@gmail.com>
2025-09-01 19:11:49 +0600

    test

--- UserManagementSystemServer/index.js
+++ UserManagementSystemServer/index.js
@@ -4,7 +4,7 @@ const app = express();
 const cors = require('cors')
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;   
 
 // middleware
 app.use(cors())
